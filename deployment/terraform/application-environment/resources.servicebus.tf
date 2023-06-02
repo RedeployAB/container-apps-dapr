18a301005170548240505674c62f26f21850c174
Karl Wallenius <karl.wallenius@redeploy.com>
2023-06-02 16:08:40 +0200

    ci: Update Terraform to handle queue and pubsub infrastructure for DAPR

--- deployment/terraform/application-environment/resources.servicebus.tf
+++ deployment/terraform/application-environment/resources.servicebus.tf
@@ -1,3 +1,11 @@
+locals {
+  servicebus_namespace_authorization_rule_name = (
+    var.messaging_system == "queue"
+    ? "queue-scaling"
+    : "topic-scaling"
+  )
+}
+
 resource "azurerm_servicebus_namespace" "main" {
   name                = var.servicebus_name
   resource_group_name = local.resource_group.name
@@ -7,19 +15,28 @@ resource "azurerm_servicebus_namespace" "main" {
   sku = "Standard"
 }
 
-resource "azurerm_servicebus_topic" "create" {
-  name         = "create"
-  namespace_id = azurerm_servicebus_namespace.main.id
-}
-
-resource "azurerm_role_assignment" "pubsub" {
+resource "azurerm_role_assignment" "servicebus" {
   scope                = azurerm_servicebus_namespace.main.id
   role_definition_name = "Azure Service Bus Data Owner"
   principal_id         = azurerm_user_assigned_identity.main.principal_id
 }
 
-resource "azurerm_servicebus_namespace_authorization_rule" "pubsub_scaling" {
-  name         = var.servicebus_namespace_authorization_rule_name
+resource "azurerm_servicebus_queue" "create" {
+  count = var.messaging_system == "queue" ? 1 : 0
+
+  name         = "create"
+  namespace_id = azurerm_servicebus_namespace.main.id
+}
+
+resource "azurerm_servicebus_topic" "create" {
+  count = var.messaging_system == "pubsub" ? 1 : 0
+
+  name         = "create"
+  namespace_id = azurerm_servicebus_namespace.main.id
+}
+
+resource "azurerm_servicebus_namespace_authorization_rule" "scaling" {
+  name         = local.servicebus_namespace_authorization_rule_name
   namespace_id = azurerm_servicebus_namespace.main.id
 
   manage = true
